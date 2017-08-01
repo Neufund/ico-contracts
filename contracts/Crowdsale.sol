4b1dba5ad3b8c30c30fe795b2ee09b257cb4a764
Moe <mohammed.elsharif@gmail.com>
2017-08-01 18:32:04 +0200

    -Setup ES6 Mocha test for truffle -Clean test helper folder -Changes in Crowdsale contract

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.11;
 
-import '../token/MintableToken.sol';
-import '../math/SafeMath.sol';
+import 'zeppelin-solidity/contracts/token/MintableToken.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 
 /**
  * @title Crowdsale
--- test/helpers/ether.js
+++ test/helpers/ether.js
@@ -1,3 +1,5 @@
+import web3 from 'web3';
+
 export default function ether(n) {
-  return new web3.BigNumber(web3.toWei(n, 'ether'))
+  return new web3.BigNumber(web3.toWei(n, 'ether'));
 }
