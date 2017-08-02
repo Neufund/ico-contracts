dd4339fdf3c8a80ad1166565a5ac63c1980ed8bb
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-08-02 09:19:43 +0200

    modified EtherToken to conform to MutableToken, used zeppeling for base classes

--- contracts/EtherToken.sol
+++ contracts/EtherToken.sol
@@ -1,45 +1,23 @@
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.11;
 
-import ""
+import 'zeppelin-solidity/contracts/token/ERC20Basic.sol';
+import 'zeppelin-solidity/contracts/token/BasicToken.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 
-contract ERC20Basic {
-    uint256 public totalSupply;
-    function balanceOf(address who) constant returns (uint256);
-    function transfer(address to, uint256 value) returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-}
 
 contract MutableToken is ERC20Basic {
-    function deposit(address to, uint256 amount) returns (bool);
+    // todo: do smth about payable which is force to be part of interface by compiler!
+    function deposit(address to, uint256 amount) payable returns (bool);
     function withdraw(uint256 amount);
+
+    // EVENTS
+
+    event Deposit(address indexed who, uint amount);
+    event Withdrawal(address indexed who, uint amount);
 }
 
 library Math {
-    function mul(uint256 a, uint256 b) internal constant returns (uint256) {
-        uint256 c = a * b;
-        assert(a == 0 || c / a == b);
-        return c;
-    }
-
-    function div(uint256 a, uint256 b) internal constant returns (uint256) {
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-        return c;
-    }
-
-    function sub(uint256 a, uint256 b) internal constant returns (uint256) {
-        assert(b <= a);
-        return a - b;
-    }
-
-    function add(uint256 a, uint256 b) internal constant returns (uint256) {
-        uint256 c = a + b;
-        assert(c >= a);
-        return c;
-    }
-
-    // todo: should be a library
+    // todo: propose changes to zeppeling, I want this function
     function divRound(uint v, uint d) public constant returns(uint) {
         // round up if % is half or more
         return (v + (d/2)) / d;
@@ -55,8 +33,8 @@ library Math {
 /// @author Melonport AG <team@melonport.com>
 /// @notice Make Ether into a ERC20 compliant token
 /// @notice Compliant to https://github.com/nexusdev/dappsys/blob/04451acf23f017beecb1a4cad4702deadc929811/contracts/token/base.sol
-contract EtherToken is MutableToken {
-    using Math for uint256;
+contract EtherToken is BasicToken, MutableToken {
+    using SafeMath for uint256;
 
     // FIELDS
 
@@ -65,22 +43,11 @@ contract EtherToken is MutableToken {
     string public constant symbol = "ETH-T";
     uint public constant decimals = 18;
 
-    // EVENTS
-
-    event Deposit(address indexed who, uint amount);
-    event Withdrawal(address indexed who, uint amount);
-
     // METHODS
 
     modifier balances_msg_sender_at_least(uint x) {
         assert(balances[msg.sender] >= x);
         _;
-    }
-
-    // NON-CONSTANT METHODS
-
-    function EtherToken()
-    {
     }
 
     // disable default function
@@ -91,23 +58,24 @@ contract EtherToken is MutableToken {
     function deposit(address to, uint256 amount)
         payable
         // onlyDepositors
+        public
         returns (bool)
     {
         // must have as much ether as declared
-        requires(msg.value == amount);
+        require(msg.value == amount);
         balances[msg.sender] = balances[msg.sender].add(msg.value);
-        Deposit(msg.sender, msg.value);
+        Deposit(to, amount);
         return true;
     }
 
     /// Post: Exchanged Token against Ether
-    function withdraw(uint amount)
+    /// always withdraw to the sender!
+    function withdraw(uint256 amount)
         balances_msg_sender_at_least(amount)
-        returns (bool)
+        public
     {
-        balances[msg.sender] = balances[msg.sender].add(amount);
+        balances[msg.sender] = balances[msg.sender].sub(amount);
         assert(msg.sender.send(amount));
         Withdrawal(msg.sender, amount);
-        return true;
     }
 }
