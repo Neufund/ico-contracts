65b58ce7411d80443ef0f812479d96d9849e9359
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-08-02 15:59:12 +0200

    simple test for crowdsale

--- test/Crowdsale.js
+++ test/Crowdsale.js
@@ -2,6 +2,10 @@ import ether from './helpers/ether';
 import advanceToBlock from './helpers/advanceToBlock';
 import EVMThrow from './helpers/EVMThrow';
 
+const Crowdsale = artifacts.require('Crowdsale');
+const LockedAccount = artifacts.require('LockedAccount');
+const EtherToken = artifacts.require('EtherToken');
+
 const BigNumber = web3.BigNumber;
 
 const should = require('chai')
@@ -9,135 +13,11 @@ const should = require('chai')
   .use(require('chai-bignumber')(BigNumber))
   .should();
 
-const Crowdsale = artifacts.require('Crowdsale');
-const MintableToken = artifacts.require('MintableToken');
-
-contract('Crowdsale', ([_, investor, wallet, purchaser]) => {
-  const rate = new BigNumber(1000);
-  const value = ether(1);
-
-  const expectedTokenAmount = rate.mul(value);
-
-  beforeEach(async function () {
-    this.startBlock = new Date(Date.now()).getTime() / 1000
-;
-    this.endBlock = new Date(Date.now()).getTime() / 1000;
-
-    this.crowdsale = await Crowdsale.new(this.startBlock, this.endBlock, rate, wallet);
-
-    this.token = MintableToken.at(await this.crowdsale.token.call());
-  });
-
-  it('should be token owner', async function () {
-    const test = await this.crowdsale.test.call();
-    console.log(test);
-    const date = new Date(test * 1000);
-    console.log(date.toLocaleTimeString());
-    console.log(date.toLocaleDateString());
-    const timestamp = new Date(date).getTime() / 1000;
-    console.log(timestamp);
-    const owner = await this.token.owner();
-    owner.should.equal(this.crowdsale.address);
-  });
-
-  it('should be ended only after end', async function () {
-    let ended = await this.crowdsale.hasEnded();
-    ended.should.equal(false);
-    await advanceToBlock(this.endBlock + 1);
-    ended = await this.crowdsale.hasEnded();
-    ended.should.equal(true);
-  });
-
-  describe('accepting payments', () => {
-    it('should reject payments before start', async function () {
-      await this.crowdsale.send(value).should.be.rejectedWith(EVMThrow);
-      await this.crowdsale.buyTokens(investor, { from: purchaser, value }).should.be.rejectedWith(EVMThrow);
-    });
-
-    it('should accept payments after start', async function () {
-      await advanceToBlock(this.startBlock - 1);
-      await this.crowdsale.send(value).should.be.fulfilled;
-      await this.crowdsale.buyTokens(investor, { value, from: purchaser }).should.be.fulfilled;
-    });
-
-    it('should reject payments after end', async function () {
-      await advanceToBlock(this.endBlock);
-      await this.crowdsale.send(value).should.be.rejectedWith(EVMThrow);
-      await this.crowdsale.buyTokens(investor, { value, from: purchaser }).should.be.rejectedWith(EVMThrow);
-    });
-  });
-
-  describe('high-level purchase', () => {
-    beforeEach(async function () {
-      await advanceToBlock(this.startBlock);
-    });
-
-    it('should log purchase', async function () {
-      const { logs } = await this.crowdsale.sendTransaction({ value, from: investor });
-
-      const event = logs.find(e => e.event === 'TokenPurchase');
-
-      should.exist(event);
-      event.args.purchaser.should.equal(investor);
-      event.args.beneficiary.should.equal(investor);
-      event.args.value.should.be.bignumber.equal(value);
-      event.args.amount.should.be.bignumber.equal(expectedTokenAmount);
-    });
-
-    it('should increase totalSupply', async function () {
-      await this.crowdsale.send(value);
-      const totalSupply = await this.token.totalSupply();
-      totalSupply.should.be.bignumber.equal(expectedTokenAmount);
-    });
-
-    it('should assign tokens to sender', async function () {
-      await this.crowdsale.sendTransaction({ value, from: investor });
-      const balance = await this.token.balanceOf(investor);
-      balance.should.be.bignumber.equal(expectedTokenAmount);
-    });
-
-    it('should forward funds to wallet', async function () {
-      const pre = web3.eth.getBalance(wallet);
-      await this.crowdsale.sendTransaction({ value, from: investor });
-      const post = web3.eth.getBalance(wallet);
-      post.minus(pre).should.be.bignumber.equal(value);
-    });
-  });
-
-  describe('low-level purchase', () => {
-    beforeEach(async function () {
-      await advanceToBlock(this.startBlock);
-    });
-
-    it('should log purchase', async function () {
-      const { logs } = await this.crowdsale.buyTokens(investor, { value, from: purchaser });
-
-      const event = logs.find(e => e.event === 'TokenPurchase');
-
-      should.exist(event);
-      event.args.purchaser.should.equal(purchaser);
-      event.args.beneficiary.should.equal(investor);
-      event.args.value.should.be.bignumber.equal(value);
-      event.args.amount.should.be.bignumber.equal(expectedTokenAmount);
-    });
-
-    it('should increase totalSupply', async function () {
-      await this.crowdsale.buyTokens(investor, { value, from: purchaser });
-      const totalSupply = await this.token.totalSupply();
-      totalSupply.should.be.bignumber.equal(expectedTokenAmount);
-    });
-
-    it('should assign tokens to beneficiary', async function () {
-      await this.crowdsale.buyTokens(investor, { value, from: purchaser });
-      const balance = await this.token.balanceOf(investor);
-      balance.should.be.bignumber.equal(expectedTokenAmount);
-    });
-
-    it('should forward funds to wallet', async function () {
-      const pre = web3.eth.getBalance(wallet);
-      await this.crowdsale.buyTokens(investor, { value, from: purchaser });
-      const post = web3.eth.getBalance(wallet);
-      post.minus(pre).should.be.bignumber.equal(value);
-    });
+contract(Crowdsale, ([_, investor, wallet, purchaser]) => {
+  it('should be able to read ICO parameters', async () => {
+    const instance = await Crowdsale.deployed();
+    assert.equal(await instance.startDate.call(), 1501681287);
+    assert.equal(await instance.ownedToken.call(), EtherToken.address);
+    assert.equal(await instance.lockedAccount.call(), LockedAccount.address);
   });
 });
