0920bfcfad83fc463001b6c6682bb5cf4ad30120
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-08-02 14:25:09 +0200

    deploy all contracts, except crowdsale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,23 +1,36 @@
-const ConvertLib = artifacts.require('./ConvertLib.sol');
-const MetaCoin = artifacts.require('./MetaCoin.sol');
 const NeumarkFactory = artifacts.require('./NeumarkFactory.sol');
 const NeumarkController = artifacts.require('./NeumarkController.sol');
 const Neumark = artifacts.require('./Neumark.sol');
+const LockedAccount = artifacts.require('LockedAccount');
+const SafeMath = artifacts.require('SafeMath');
+const EtherToken = artifacts.require('EtherToken');
+const Crowdsale = artifacts.require('Crowdsale');
+
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const months = 30 * 24 * 60 * 60;
+const FP_SCALE = 10000;
 
 module.exports = deployer =>
   deployer.then(async () => {
-    console.log('Constracts deploying...');
-    await deployer.deploy(ConvertLib);
-    await deployer.link(ConvertLib, MetaCoin);
-    await deployer.deploy(MetaCoin);
+    console.log('Neumark deploying...');
+    await deployer.deploy(SafeMath);
     await deployer.deploy(NeumarkFactory);
     await deployer.deploy(Neumark, NeumarkFactory.address);
     await deployer.deploy(NeumarkController, Neumark.address);
     const neumark = await Neumark.deployed();
     await neumark.changeController(NeumarkController.address);
-
+    console.log('ETR-T and LockedAccount deploying...')
+    await deployer.deploy(EtherToken);
+    await deployer.link(SafeMath, EtherToken);
+    const etherToken = await EtherToken.deployed();
+    await deployer.link(SafeMath, LockedAccount);
+    await deployer.deploy(LockedAccount, etherToken.address, neumark.address, 18 * months, Math.round(0.1 * FP_SCALE));
+    const lock = await LockedAccount.deployed();
+    console.log('Deploying crowdsale')
+    // await deployer.deploy(Crowdsale, etherToken.address, neumark.address, lock.address);
+    // const crowdsale = await Crowdsale.deployed();
+    // await lock.setController(crowdsale);
     //await sleep(10000); // Verify that Truffle actually waits for the promise to complete.
     console.log('Contracts deployed!');
   });
