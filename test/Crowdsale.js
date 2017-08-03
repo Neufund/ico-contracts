0cabe451064ab4b5307a28ee61f0a2748217aa62
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-08-03 12:08:25 +0200

    fail commitment test via _changeEndDate

--- test/Crowdsale.js
+++ test/Crowdsale.js
@@ -18,11 +18,28 @@ const should = require('chai')
 const money = new ether(1);
 
 contract(Crowdsale, () => {
-  it('should be able to read ICO parameters', async () => {
+  it('should be able to read Commitment parameters', async () => {
     const instance = await Crowdsale.deployed();
-  //  assert.equal(await instance.startDate.call(), 1501681287);
+    assert.equal(await instance.startDate.call(), 1501804800);
     assert.equal(await instance.ownedToken.call(), EtherToken.address);
     assert.equal(await instance.lockedAccount.call(), LockedAccount.address);
     assert.equal(await instance.curve.call(), Curve.address);
+  });
+
+  it('should complete Commitment with failed state', async () => {
+    const instance = await Crowdsale.deployed();
+    const lock = await LockedAccount.deployed();
+    assert.equal(await lock.lockState.call(), 1, 'lock should be in AcceptingLocks');
+    const timestamp = await instance.currentTime();
+    assert.equal(await instance.hasEnded.call(), false, 'commitment should run');
+    console.log(`obtained timestamp ${timestamp}`);
+    // make commitment finish due to end date
+    await instance._changeEndDate(timestamp - 1);
+    assert.equal(await instance.hasEnded.call(), true, 'commitment should end');
+    assert.equal(await instance.wasSuccessful.call(), false, 'commitment should fail');
+    // now finalize
+    await instance.finalize();
+    // check lock state
+    assert.equal(await lock.lockState.call(), 3, 'lock should be in ReleaseAll');
   });
 });
