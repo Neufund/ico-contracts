df9bef636330e06ef3965dd7f222fbf9e1863421
Remco Bloemen <remco-git@xn--2-umb.com>
2017-08-08 15:28:45 +0200

    Add error handling contract

---
+++ contracts/Error.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.11;
+
+contract Error {
+
+    enum Status {
+        SUCCESS,
+        Status,
+        /// @dev Add your own
+    }
+
+    event Error(Status code);
+
+    /// Example usage:
+    ///
+    /// if(msg.value < expected)
+    ///     return logError(Status.Status);
+    ///
+    function logError(Status code)
+        internal
+        returns (Status)
+    {
+        Error(code);
+        return code;
+    }
+}
