036e093a6a3f4349b2c641a31bb3a8ab882ed54a
Remco Bloemen <remco-git@xn--2-umb.com>
2017-08-09 10:26:34 +0200

    Quickcheck

---
+++ test/quickCheck.js
@@ -0,0 +1,7 @@
+import { extrema } from './helpers/quickCheck';
+
+describe('quickCheck', () => {
+  it('should ', async () => {
+    Object.keys(extrema).map(k => console.log(`${k}\t= ${extrema[k].toString(16)}`));
+  });
+});
