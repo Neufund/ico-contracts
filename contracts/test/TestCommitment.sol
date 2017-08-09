a8d15ac44f503a06cb8c1b5eedcbfcbd0996ac33
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-08-10 00:19:27 +0200

    TestCommitment contract extends PublicOffering, manipulation of internal state

---
+++ contracts/test/TestCommitment.sol
@@ -0,0 +1,57 @@
+pragma solidity ^0.4.11;
+
+import '../PublicCommitment.sol';
+
+contract TestCommitment is PublicCommitment {
+
+    function _succ() {
+        lockedAccount.controllerSucceeded();
+    }
+
+    function _fail() {
+        lockedAccount.controllerFailed();
+    }
+
+    // a test function to change start date of ICO - may be useful for UI demo
+    function _changeStartDate(uint256 date)
+        public
+    {
+        startDate = date;
+    }
+
+    // a test function to change start date of ICO - may be useful for UI demo
+    function _changeEndDate(uint256 date)
+        public
+    {
+        endDate = date;
+    }
+
+    function _changeMaxCap(uint256 _cap)
+        public
+    {
+        maxCap = _cap;
+    }
+
+    function _changeMinCap(uint256 _cap)
+        public
+    {
+        minCap = _cap;
+    }
+
+    function _investFor(address investor, uint256 amount, uint256 neumarks)
+        payable
+    {
+        // mint new ETH-T for yourself
+        require(paymentToken.deposit.value(msg.value)(address(this), amount));
+        // make allowance for lock
+        require(paymentToken.approve(address(lockedAccount), amount));
+        // lock in lock
+        lockedAccount.lock(investor, amount, neumarks);
+    }
+
+    function TestCommitment(uint256 _startDate, uint256 _endDate, uint256 _minCommitment,
+         uint256 _maxCommitment, TokenWithDeposit _ethToken, LockedAccount _lockedAccount, Curve _curve)
+         PublicCommitment(_startDate, _endDate, _minCommitment, _maxCommitment, _ethToken, _lockedAccount, _curve)
+    {
+    }
+}
