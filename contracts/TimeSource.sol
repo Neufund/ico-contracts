aca96858a52b5cdcd723af3f1b07709d94c1c736
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-08-15 16:48:25 +0200

    removed mockTime from TimeSource contract

--- contracts/TimeSource.sol
+++ contracts/TimeSource.sol
@@ -1,16 +1,7 @@
 pragma solidity ^0.4.11;
 
 contract TimeSource {
-    uint256 private mockNow;
-
-    function currentTime() public constant returns (uint256) {
-        return mockNow > 0 ? mockNow : block.timestamp;
-    }
-
-    function mockTime(uint256 t) public {
-        // no mocking on mainnet
-        if (block.number > 3316029)
-            revert();
-        mockNow = t;
+    function currentTime() internal constant returns (uint256) {
+        return block.timestamp;
     }
 }
