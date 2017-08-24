05109223ce16c6952a7cfbd5ee10b5a1b24d06e9
Remco Bloemen <remco-git@xn--2-umb.com>
2017-08-24 18:06:00 +0200

    Use Standards in Zeppelin

--- contracts/Zeppelin/BasicToken.sol
+++ contracts/Zeppelin/BasicToken.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.11;
 
 
-import './ERC20Basic.sol';
+import '../Standards/IBasicToken.sol';
 import './SafeMath.sol';
 
 
@@ -9,7 +9,7 @@ import './SafeMath.sol';
  * @title Basic token
  * @dev Basic version of StandardToken, with no allowances.
  */
-contract BasicToken is ERC20Basic {
+contract BasicToken is IBasicToken {
   using SafeMath for uint256;
 
   mapping(address => uint256) balances;
