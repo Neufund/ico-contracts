3ba474d4de0cad70beec7ad27220478155ffc6cc
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-08-24 14:08:58 +0200

    deploy access controller and set roles in deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,14 @@
 require("babel-register");
 
-const NeumarkController = artifacts.require("./NeumarkController.sol");
-const Neumark = artifacts.require("./Neumark.sol");
+const NeumarkController = artifacts.require("NeumarkController");
+const Neumark = artifacts.require("Neumark");
 const LockedAccount = artifacts.require("LockedAccount");
 const SafeMath = artifacts.require("SafeMath");
 const EtherToken = artifacts.require("EtherToken");
 const PublicCommitment = artifacts.require("PublicCommitment");
-const Curve = artifacts.require("./Curve.sol");
+const Curve = artifacts.require("Curve");
+const RoleBasedAccessControl = artifacts.require("RoleBasedAccessControl");
+const AccessRoles = artifacts.require("AccessRoles");
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 const months = 30 * 24 * 60 * 60;
@@ -17,8 +19,11 @@ const ether = Wei => Wei * 10 ** 18;
 const maxCap = new web3.BigNumber(web3.toWei(30, 'ether'));
 const startDate = Date.now.getTime() / 1000; */
 
-module.exports = deployer =>
+module.exports = function(deployer, network, accounts) {
   deployer.then(async () => {
+    console.log("AccessControl deployment...");
+    await deployer.deploy(RoleBasedAccessControl);
+    const accessControl = await RoleBasedAccessControl.deployed();
     console.log("Neumark deploying...");
     await deployer.deploy(Neumark);
     await deployer.deploy(NeumarkController, Neumark.address);
@@ -32,6 +37,7 @@ module.exports = deployer =>
       LockedAccount,
       etherToken.address,
       Curve.address,
+      accessControl.address,
       18 * months,
       Math.round(0.1 * ether(1)) // fractions are in 10**18
     );
@@ -51,10 +57,18 @@ module.exports = deployer =>
     );
     const publicCommitment = await PublicCommitment.deployed();
     console.log("Commitment deployed");
-    await lock.setController(publicCommitment.address);
+    console.log("Seting permissions");
+    await deployer.deploy(AccessRoles);
+    const accessRoles = await AccessRoles.deployed();
+    await accessControl.setUserRole(accounts[1], await accessRoles.ROLE_LOCKED_ACCOUNT_ADMIN(),
+      lock.address, 1); // 1 is True
+    await lock.setController(publicCommitment.address, {from: accounts[1]});
+    await accessControl.setUserRole(accounts[2], await accessRoles.ROLE_WHITELIST_ADMIN(),
+      PublicCommitment.address, 1);
     console.log("Contracts deployed!");
 
     console.log("----------------------------------");
     console.log(`ICO contract: ${publicCommitment.address}`);
     console.log("----------------------------------");
   });
+}
