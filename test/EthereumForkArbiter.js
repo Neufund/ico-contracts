96d4b6e4f5b294184375875b2258511df9b13c4c
Remco Bloemen <remco-git@xn--2-umb.com>
2017-08-29 18:51:28 +0200

    Use revert and respectGasLimit

--- test/EthereumForkArbiter.js
+++ test/EthereumForkArbiter.js
@@ -2,7 +2,6 @@ import { expect } from "chai";
 import { prettyPrintGasCost } from "./helpers/gasUtils";
 import createAccessPolicy from "./helpers/createAccessPolicy";
 import forceEther from "./helpers/forceEther";
-import expectThrow from "./helpers/expectThrow";
 import { eventValue } from "./helpers/events";
 
 const EthereumForkArbiter = artifacts.require("EthereumForkArbiter");
@@ -19,109 +18,131 @@ contract("EthereumForkArbiter", ([deployer, arbiter, other]) => {
 
   it("should deploy", async () => {
     prettyPrintGasCost("Deploy", ethereumForkArbiter);
+    expect(ethereumForkArbiter).to.respectGasLimit(140000);
   });
 
   it("should announce forks", async () => {
     const name = "Spurious Dragon";
     const url =
       "https://blog.ethereum.org/2016/11/18/hard-fork-no-4-spurious-dragon/";
+
     const tx = await ethereumForkArbiter.announceFork(name, url, {
       from: arbiter
     });
+
+    prettyPrintGasCost("Announce", tx);
     expect(eventValue(tx, "ForkAnnounced", "name")).to.equal(name);
     expect(eventValue(tx, "ForkAnnounced", "url")).to.equal(url);
-    prettyPrintGasCost("Announce", tx);
+    expect(tx).to.respectGasLimit(140000);
   });
 
   it("should remember last announced fork", async () => {
-    const name = "Spurious Dragon";
-    const url =
+    const expectedName = "Spurious Dragon";
+    const expectedUrl =
       "https://blog.ethereum.org/2016/11/18/hard-fork-no-4-spurious-dragon/";
-    const tx = await ethereumForkArbiter.announceFork(name, url, {
-      from: arbiter
-    });
-    const rName = await ethereumForkArbiter.nextForkName.call();
-    const rUrl = await ethereumForkArbiter.nextForkUrl.call();
-    expect(rName).to.equal(name);
-    expect(rUrl).to.equal(url);
+
+    const tx = await ethereumForkArbiter.announceFork(
+      expectedName,
+      expectedUrl,
+      { from: arbiter }
+    );
+    const actualName = await ethereumForkArbiter.nextForkName.call();
+    const actualUrl = await ethereumForkArbiter.nextForkUrl.call();
+
+    expect(actualName).to.equal(expectedName);
+    expect(actualUrl).to.equal(expectedUrl);
   });
 
   it("should sign forks", async () => {
     const block = await web3.eth.getBlock("latest");
+
     const tx = await ethereumForkArbiter.signFork(block.number, block.hash, {
       from: arbiter
     });
+
+    prettyPrintGasCost("Sign", tx);
     expect(eventValue(tx, "ForkSigned", "blockNumber")).to.be.bignumber.equal(
       block.number
     );
     expect(eventValue(tx, "ForkSigned", "blockHash")).to.be.equal(block.hash);
-    prettyPrintGasCost("Sign", tx);
+    expect(tx).to.respectGasLimit(100000);
   });
 
   it("should check hash of signed fork", async () => {
     const block = await web3.eth.getBlock("latest");
     const hash =
       "0x8693c7c1ec855e1ef02fb45536ea545b0c3fc137d700dce21300a8254423d8a4";
-    await expectThrow(
+
+    await expect(
       ethereumForkArbiter.signFork(block.number, hash, {
         from: arbiter
       })
-    );
-    await ethereumForkArbiter.signFork(block.number, block.hash, {
-      from: arbiter
-    });
+    ).to.revert;
+    await expect(
+      ethereumForkArbiter.signFork(block.number, block.hash, {
+        from: arbiter
+      })
+    ).to.not.revert;
   });
 
   it("should remember last signed fork", async () => {
-    const block = await web3.eth.getBlock("latest");
-    const tx = await ethereumForkArbiter.signFork(block.number, block.hash, {
-      from: arbiter
-    });
-    const rNumber = await ethereumForkArbiter.lastSignedBlockNumber.call();
-    const rHash = await ethereumForkArbiter.lastSignedBlockHash.call();
-    const rTime = await ethereumForkArbiter.lastSignedTimestamp.call();
-    expect(rNumber).to.be.bignumber.equal(block.number);
-    expect(rHash).to.equal(block.hash);
-    const rBlock = await web3.eth.getBlock(tx.receipt.blockNumber);
-    const time = rBlock.timestamp;
-    expect(rNumber).to.be.bignumber.equal(block.number);
-    expect(rHash).to.be.bignumber.equal(block.hash);
-    expect(rTime).to.be.bignumber.equal(time);
+    const latestBlock = await web3.eth.getBlock("latest");
+
+    const tx = await ethereumForkArbiter.signFork(
+      latestBlock.number,
+      latestBlock.hash,
+      { from: arbiter }
+    );
+    const actualNumber = await ethereumForkArbiter.lastSignedBlockNumber.call();
+    const actualHash = await ethereumForkArbiter.lastSignedBlockHash.call();
+    const actualTime = await ethereumForkArbiter.lastSignedTimestamp.call();
+    const txBlock = await web3.eth.getBlock(tx.receipt.blockNumber);
+    const expectedTime = txBlock.timestamp;
+
+    expect(actualNumber).to.be.bignumber.equal(latestBlock.number);
+    expect(actualHash).to.be.bignumber.equal(latestBlock.hash);
+    expect(actualTime).to.be.bignumber.equal(expectedTime);
   });
 
   it("should only allow ROLE_FORK_ARBITER to announce", async () => {
     const name = "Spurious Dragon";
     const url =
       "https://blog.ethereum.org/2016/11/18/hard-fork-no-4-spurious-dragon/";
-    await expectThrow(
+
+    await expect(
       ethereumForkArbiter.announceFork(name, url, {
         from: deployer
       })
-    );
-    await expectThrow(
+    ).to.revert;
+    await expect(
       ethereumForkArbiter.announceFork(name, url, {
         from: other
       })
-    );
-    await ethereumForkArbiter.announceFork(name, url, {
-      from: arbiter
-    });
+    ).to.revert;
+    await expect(
+      ethereumForkArbiter.announceFork(name, url, {
+        from: arbiter
+      })
+    ).to.not.revert;
   });
 
   it("should only allow ROLE_FORK_ARBITER to sign", async () => {
     const block = await web3.eth.getBlock("latest");
-    await expectThrow(
+
+    await expect(
       ethereumForkArbiter.signFork(block.number, block.hash, {
         from: deployer
       })
-    );
-    await expectThrow(
+    ).to.revert;
+    await expect(
       ethereumForkArbiter.signFork(block.number, block.hash, {
         from: other
       })
-    );
-    await ethereumForkArbiter.signFork(block.number, block.hash, {
-      from: arbiter
-    });
+    ).to.revert;
+    await expect(
+      ethereumForkArbiter.signFork(block.number, block.hash, {
+        from: arbiter
+      })
+    ).to.not.revert;
   });
 });
