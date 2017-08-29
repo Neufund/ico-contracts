f38098f5cfc8078dd6a5d5c7c321363bf39ee6f0
Remco Bloemen <remco-git@xn--2-umb.com>
2017-08-29 16:44:19 +0200

    String arrays not allowed

--- contracts/Agreement.sol
+++ contracts/Agreement.sol
@@ -3,14 +3,9 @@ pragma solidity 0.4.15;
 
 contract Agreement {
 
-    string[] public agreementMimeTypes;
-    mapping(string => string) public agreementByType;
+    string public agreementUri;
 
-    function Agreement(byte[255][] mimeTypes, byte[255][] uris) {
-        require(mimeTypes.length == uris.length);
-        for (uint256 i = 0; i < mimeTypes.length; i++) {
-            agreementByType[mimeTypes[i]] = uris[i];
-        }
-        agreementMimeTypes = mimeTypes;
+    function Agreement(string uri) {
+        agreementUrl = uri;
     }
 }
