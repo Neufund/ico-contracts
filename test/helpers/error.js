2a401e1f243d602261e2b1894abedbea40cd2188
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-08-30 01:07:04 +0200

    adds error codes to error event helper

--- test/helpers/error.js
+++ test/helpers/error.js
@@ -1,9 +1,15 @@
 import { hasEvent, eventValue } from "./events";
 
+export const Status = Object.freeze({
+  SUCCESS: 0,
+  NOT_ENOUGH_NEUMARKS_TO_UNLOCK: 1,
+  INSUFFICIENT_FUNDS: 2
+});
 export default function error(tx) {
   // Default to zero on no error
   if (!hasEvent(tx, "Error")) {
     return 0;
   }
+
   return parseInt(eventValue(tx, "Error", "code"), 10);
 }
