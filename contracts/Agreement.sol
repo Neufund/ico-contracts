831877561a51ae3c390587f08540dc7fa406b10f
Remco Bloemen <remco-git@xn--2-umb.com>
2017-08-30 14:02:16 +0200

    Add fork arbiter to agreement

--- contracts/Agreement.sol
+++ contracts/Agreement.sol
@@ -1,11 +1,16 @@
 pragma solidity 0.4.15;
 
+import './Standards/IEthereumForkArbiter.sol';
+
 
 contract Agreement {
 
+    IEthereumForkArbiter ethereumForkArbiter;
     string public agreementUri;
 
-    function Agreement(string uri) {
-        agreementUrl = uri;
+    function Agreement(IEthereumForkArbiter forkArbiter, string uri) {
+        require(forkArbiter != IEthereumForkArbiter(0x0));
+        ethereumForkArbiter = forkArbiter;
+        agreementUri = uri;
     }
 }
