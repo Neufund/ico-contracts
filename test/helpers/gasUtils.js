f8014a7f3fae4247414e04ea2105cef69e32aa95
Remco Bloemen <remco-git@xn--2-umb.com>
2017-08-31 23:13:53 +0200

    Asynchronous gas cost

--- test/helpers/gasUtils.js
+++ test/helpers/gasUtils.js
@@ -7,21 +7,29 @@ const gasCostString = gas =>
     1000 * gas / gasLimit
   ) / 10}% of limit)`;
 
-const txGasCost = tx => tx.receipt.gasUsed;
+export const txGasCost = tx => tx.receipt.gasUsed;
 
-const contractGasCost = contract =>
-  web3.eth.getTransactionReceipt(contract.transactionHash).gasUsed;
+export const promisify = func => async (...args) =>
+  new Promise((accept, reject) =>
+    func(...args, (error, result) => (error ? reject(error) : accept(result)))
+  );
 
-export const gasCost = obj => {
+export const gasCost = async obj => {
   if (Number.isInteger(obj)) {
     return obj;
   }
+  if (obj.receipt) {
+    return obj.receipt.gasUsed;
+  }
 
-  return obj.receipt ? txGasCost(obj) : contractGasCost(obj);
+  const receipt = await promisify(web3.eth.getTransactionReceipt)(
+    obj.transactionHash
+  );
+  return receipt.gasUsed;
 };
 
-export const prettyPrintGasCost = (what, obj) => {
-  const gas = gasCost(obj);
+export const prettyPrintGasCost = async (what, obj) => {
+  const gas = await gasCost(obj);
 
   // eslint-disable-next-line no-console
   console.log(`\t${what} took ${gasCostString(gas)}`);
