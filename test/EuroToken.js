f83ac727619eb8a0cdb4ed8154e585d9713f4673
Remco Bloemen <remco-git@xn--2-umb.com>
2017-09-13 17:36:03 +0200

    Test EuroToken deploy

---
+++ test/EuroToken.js
@@ -0,0 +1,26 @@
+import { prettyPrintGasCost } from "./helpers/gasUtils";
+import createAccessPolicy from "./helpers/createAccessPolicy";
+import { saveBlockchain, restoreBlockchain } from "./helpers/evmCommands";
+
+const EuroToken = artifacts.require("./EuroToken.sol");
+
+contract("EuroToken", () => {
+  let snapshot;
+  let rbac;
+  let euroToken;
+
+  before(async () => {
+    rbac = await createAccessPolicy([]);
+    euroToken = await EuroToken.new(rbac);
+    snapshot = await saveBlockchain();
+  });
+
+  beforeEach(async () => {
+    await restoreBlockchain(snapshot);
+    snapshot = await saveBlockchain();
+  });
+
+  it("should deploy", async () => {
+    await prettyPrintGasCost("EuroToken deploy", euroToken);
+  });
+});
