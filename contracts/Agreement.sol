2e47c0881f85cb0cbdedfb1fd9936a2aec06a09f
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-09-13 19:16:23 +0200

    adds mapping of signatories to Agreement and removes cancel functionality

--- contracts/Agreement.sol
+++ contracts/Agreement.sol
@@ -8,20 +8,20 @@ import './AccessRoles.sol';
 /**
  * @title legally binding smart contract
  * @dev General approach to paring legal and smart contracts:
- * 1.    All terms and agreement are between two parties: here between legal representation of platform operator and platform user.
- * 2.    Parties are represented by public Ethereum address.
- * 3.    Legal agreement has immutable part that corresponds to smart contract code and mutable part that may change for example due to changing regulations or other externalities that smart contract does not control.
- * 4.    There should be a provision in legal document that future changes in mutable part cannot change terms of immutable part.
- * 5.    Immutable part links to corresponding smart contract via its address.
- * 6.    Additional provision should be added if smart contract supports it
- *  a.    Fork provision
- *  b.    Bugfixing provision (unilateral code update mechanism)
- *  c.    Migration provision (bilateral code update mechanism)
+ * 1. All terms and agreement are between two parties: here between legal representation of platform operator representative and platform investor.
+ * 2. Parties are represented by public Ethereum addresses. Platform investor is and address that holds and controls funds and receives and controls Neumark token
+ * 3. Legal agreement has immutable part that corresponds to smart contract code and mutable part that may change for example due to changing regulations or other externalities that smart contract does not control.
+ * 4. There should be a provision in legal document that future changes in mutable part cannot change terms of immutable part.
+ * 5. Immutable part links to corresponding smart contract via its address.
+ * 6. Additional provision should be added if smart contract supports it
+ *  a. Fork provision
+ *  b. Bugfixing provision (unilateral code update mechanism)
+ *  c. Migration provision (bilateral code update mechanism)
  *
  * Details on Agreement base class:
- * 1.    We bind smart contract to legal contract by storing uri (preferably ipfs or hash) of the legal contract in the smart contract. It is however crucial that such binding is done by platform operator representation so transaction establishing the link must be signed by respective wallet ('amendAgreement')
- * 2.    Mutable part of agreement may change. We should be able to amend the uri later. Previous amendments should not be lost and should be retrievable (`amendAgreement` and 'pastAgreement' functions).
- * 3.    There should be a way to opt out of the terms if they are implicit. Example: in case of Neumark any public address that receives transfer (NEU balance > 0) is implicity signing the agreement. We allow such users to opt out of Neumark Holder Agreement by burning all neumarks – that should generate rejection event. Please note that transfer by such party to ther party will explicitly sign terms so just zeroing balance is not enough.
+ * 1. We bind smart contract to legal contract by storing uri (preferably ipfs or hash) of the legal contract in the smart contract. It is however crucial that such binding is done by platform operator representation so transaction establishing the link must be signed by respective wallet ('amendAgreement')
+ * 2. Mutable part of agreement may change. We should be able to amend the uri later. Previous amendments should not be lost and should be retrievable (`amendAgreement` and 'pastAgreement' functions).
+ * 3. It is up to deriving contract to decide where to put 'acceptAgreement' modifier. However situation where there is no cryptographic proof that given address was not really acting in the transaction and sighed this address in should be avoided, simplest example being 'to' address in `transfer` function of ERC20.
  *
 **/
 contract Agreement is
@@ -36,8 +36,7 @@ contract Agreement is
     /// @notice agreement with signature of the platform operator representative
     struct SignedAgreement {
         address platformOperatorRepresentative;
-        bytes32 signedBlockHash;
-        uint256 signedTimestamp;
+        uint256 signedBlockTimestamp;
         string agreementUri;
     }
 
@@ -52,6 +51,7 @@ contract Agreement is
     ////////////////////////
 
     SignedAgreement[] private _amendments;
+    mapping(address => bool) _signatories;
 
     ////////////////////////
     // Events
@@ -59,10 +59,6 @@ contract Agreement is
 
     event LogAgreementAccepted(
         address indexed accepter
-    );
-
-    event LogAgreementCancelled(
-        address indexed rejector
     );
 
     event LogAgreementAmended(
@@ -77,16 +73,11 @@ contract Agreement is
     /// @notice logs that agreement was accepted by platform user
     /// @dev intended to be added to functions that if used make 'accepter' origin to enter legally binding agreement
     modifier acceptAgreement(address accepter) {
-        require(hasSignedAgreement());
-        LogAgreementAccepted(accepter);
-        _;
-    }
-
-    /// @notice logs that agreement was cancelled by platform user
-    /// @dev intended to be added to functions that result in cancellation of the agreement for 'rejector', see top of this file for details
-    modifier cancelAgreement(address rejector) {
-        require(hasSignedAgreement());
-        LogAgreementCancelled(rejector);
+        require(_amendments.length > 0);
+        if(!_signatories[accepter]) {
+            _signatories[accepter] = true;
+            LogAgreementAccepted(accepter);
+        }
         _;
     }
 
@@ -111,8 +102,7 @@ contract Agreement is
     {
         SignedAgreement memory amendment = SignedAgreement({
             platformOperatorRepresentative: msg.sender,
-            signedTimestamp: block.timestamp,
-            signedBlockHash: block.blockhash(block.number),
+            signedBlockTimestamp: block.timestamp,
             agreementUri: agreementUri
         });
         _amendments.push(amendment);
@@ -130,43 +120,50 @@ contract Agreement is
     function currentAgreement()
         public
         constant
-        returns (address, uint256, bytes32, string, uint256)
+        returns
+        (
+            address platformOperatorRepresentative,
+            uint256 signedBlockTimestamp,
+            string agreementUri,
+            uint256 amendmentIndex
+        )
     {
-        require(hasSignedAgreement());
-        uint256 amendmentIndex = _amendments.length - 1;
-        SignedAgreement storage amendment = _amendments[amendmentIndex];
+        require(_amendments.length > 0);
+        uint256 last = _amendments.length - 1;
+        SignedAgreement storage amendment = _amendments[last];
         return (
             amendment.platformOperatorRepresentative,
-            amendment.signedTimestamp,
-            amendment.signedBlockHash,
+            amendment.signedBlockTimestamp,
             amendment.agreementUri,
-            amendmentIndex
+            last
         );
     }
 
     function pastAgreement(uint256 amendmentIndex)
         public
         constant
-        returns (address, uint256, bytes32, string, uint256)
+        returns
+        (
+            address platformOperatorRepresentative,
+            uint256 signedBlockTimestamp,
+            string agreementUri,
+            uint256
+        )
     {
         SignedAgreement storage amendment = _amendments[amendmentIndex];
         return (
             amendment.platformOperatorRepresentative,
-            amendment.signedTimestamp,
-            amendment.signedBlockHash,
+            amendment.signedBlockTimestamp,
             amendment.agreementUri,
             amendmentIndex
         );
     }
 
-    ////////////////////////
-    // Private functions
-    ////////////////////////
-
-    function hasSignedAgreement()
-        private
-        returns (bool)
+    function isSignedBy(address signatory)
+        public
+        constant
+        returns (bool signed)
     {
-        return _amendments.length > 0;
+        return _signatories[signatory];
     }
 }
