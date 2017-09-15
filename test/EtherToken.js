1d7337712bf9e42a86086700faee3b118761ddb9
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-09-15 16:43:15 +0200

    tests EtherToken against IERC20Allowance tests

--- test/EtherToken.js
+++ test/EtherToken.js
@@ -2,9 +2,9 @@ import { expect } from "chai";
 import { prettyPrintGasCost } from "./helpers/gasUtils";
 import createAccessPolicy from "./helpers/createAccessPolicy";
 import { saveBlockchain, restoreBlockchain } from "./helpers/evmCommands";
-import { basicTokenTests } from "./helpers/tokenTestCases";
+import { basicTokenTests, standardTokenTests } from "./helpers/tokenTestCases";
+import { eventValue } from "./helpers/events";
 import ether from "./helpers/ether";
-import roles from "./helpers/roles";
 
 const EtherToken = artifacts.require("EtherToken");
 
@@ -23,14 +23,24 @@ contract("EtherToken", ([deployer, ...accounts]) => {
     snapshot = await saveBlockchain();
   });
 
+  function expectDepositEvent(tx, owner, amount) {
+    const event = eventValue(tx, "LogDeposit");
+    expect(event).to.exist;
+    expect(event.args.to).to.eq(owner);
+    expect(event.args.amount).to.be.bignumber.eq(amount);
+  }
+
   it("should deploy", async () => {
     await prettyPrintGasCost("EuroToken deploy", etherToken);
   });
 
   it("should deposit", async () => {
     const initialBalance = ether(1.19827398791827);
-    const tx = await etherToken.deposit(accounts[0], initialBalance, { from: deployer, value: initialBalance });
-    // expectDepositEvent(tx, accounts[0], initialBalance)
+    const tx = await etherToken.deposit(accounts[0], initialBalance, {
+      from: deployer,
+      value: initialBalance
+    });
+    expectDepositEvent(tx, accounts[0], initialBalance);
     const totalSupply = await etherToken.totalSupply.call();
     expect(totalSupply).to.be.bignumber.eq(initialBalance);
     const balance = await etherToken.balanceOf(accounts[0]);
@@ -43,16 +53,37 @@ contract("EtherToken", ([deployer, ...accounts]) => {
   // test deposit max value
   // test deposit overflow
 
-
   describe("IBasicToken tests", () => {
-
     const initialBalance = ether(1.19827398791827);
-    const getToken = function () { return etherToken; };
+    const getToken = () => etherToken;
 
-    beforeEach( async () => {
-      await etherToken.deposit(accounts[1], initialBalance, { from: deployer, value: initialBalance });
+    beforeEach(async () => {
+      await etherToken.deposit(accounts[1], initialBalance, {
+        from: deployer,
+        value: initialBalance
+      });
     });
 
     basicTokenTests(getToken, accounts[1], accounts[2], initialBalance);
+  });
+
+  describe("IERC20Allowance tests", () => {
+    const initialBalance = ether(1.0192);
+    const getToken = () => etherToken;
+
+    beforeEach(async () => {
+      await etherToken.deposit(accounts[1], initialBalance, {
+        from: deployer,
+        value: initialBalance
+      });
+    });
+
+    standardTokenTests(
+      getToken,
+      accounts[1],
+      accounts[2],
+      accounts[3],
+      initialBalance
+    );
   });
 });
