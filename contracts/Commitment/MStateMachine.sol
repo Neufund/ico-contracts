fadd038351adeabcd09e2276a2b71b23621bc62f
Remco Bloemen <remco-git@xn--2-umb.com>
2017-09-15 11:04:37 +0200

    Update state machine

--- contracts/Commitment/MStateMachine.sol
+++ contracts/Commitment/MStateMachine.sol
@@ -4,10 +4,31 @@ pragma solidity 0.4.15;
 contract MStateMachine {
 
     ////////////////////////
+    // Types
+    ////////////////////////
+
+    enum State {
+        Before,
+        Whitelist,
+        Pause,
+        Public,
+        Rollback,
+        Finished
+    }
+
+    ////////////////////////
     // Internal functions
     ////////////////////////
 
-    /// @notice Get's called for every state transition.
-    //      It's guaranteed that `oldState != newState`.
-    function mOnTransition(State oldState, State newState);
+    /// @notice Get's called after every state transition.
+    ///     It's guaranteed that `oldState != newState` and
+    //      `state() == oldState`.
+    function mBeforeTransition(State oldState, State newState)
+        internal;
+
+    /// @notice Get's called after every state transition.
+    ///     It's guaranteed that `oldState != newState` and
+    //      `state() == newState`.
+    function mAfterTransition(State oldState, State newState)
+        internal;
 }
