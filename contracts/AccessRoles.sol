7b988e9ae9e517e77a11107dbabdb0ba7b950284
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-09-19 22:58:04 +0200

    removes ROLE_TRANSFERER

--- contracts/AccessRoles.sol
+++ contracts/AccessRoles.sol
@@ -21,7 +21,6 @@ contract AccessRoles {
     bytes32 internal constant ROLE_NEUMARK_BURNER = 0x19ce331285f41739cd3362a3ec176edffe014311c0f8075834fdd19d6718e69f;
     bytes32 internal constant ROLE_SNAPSHOT_CREATOR = 0x08c1785afc57f933523bc52583a72ce9e19b2241354e04dd86f41f887e3d8174;
     bytes32 internal constant ROLE_TRANSFER_ADMIN = 0xb6527e944caca3d151b1f94e49ac5e223142694860743e66164720e034ec9b19;
-    bytes32 internal constant ROLE_TRANSFERER = 0x717f4dfc78f8be2df98de8d044e6a6668a3a6488162d21f64ac65d9415c2fafd;
 
     bytes32 internal constant ROLE_RECLAIMER = 0x0542bbd0c672578966dcc525b30aa16723bb042675554ac5b0362f86b6e97dc5;
 
@@ -30,6 +29,4 @@ contract AccessRoles {
 
     // allows to deposit EUR-T and allow addresses to send and receive EUR-T. keccak256("EurtDepositManager")
     bytes32 internal constant ROLE_EURT_DEPOSIT_MANAGER = 0x7c8ecdcba80ce87848d16ad77ef57cc196c208fc95c5638e4a48c681a34d4fe7;
-
-    //
 }
