f65a61608270bc8a6517431b971c80c1ad968ecf
Paweł Bylica <chfast@gmail.com>
2017-10-03 17:02:03 +0200

    Add state ordering unit test

--- test/TestStateMachine.sol
+++ test/TestStateMachine.sol
@@ -10,4 +10,34 @@ contract TestStateMachine is
     {
         assert(state() == State.Before);
     }
+
+    function testStateOrdering()
+    {
+        assert(State.Before == State.Before);
+        assert(State.Before <  State.Whitelist);
+        assert(State.Before <  State.Public);
+        assert(State.Before <  State.Finished);
+
+        assert(State.Whitelist >  State.Before);
+        assert(State.Whitelist == State.Whitelist);
+        assert(State.Whitelist <  State.Public);
+        assert(State.Whitelist <  State.Finished);
+
+        assert(State.Public >  State.Before);
+        assert(State.Public >  State.Whitelist);
+        assert(State.Public == State.Public);
+        assert(State.Public <  State.Finished);
+
+        assert(State.Finished >  State.Before);
+        assert(State.Finished >  State.Whitelist);
+        assert(State.Finished >  State.Public);
+        assert(State.Finished == State.Finished);
+
+        assert(State.Public <= State.Public);
+        assert(State.Public >= State.Public);
+
+        assert(int(State.Before) + 1 == int(State.Whitelist));
+        assert(int(State.Whitelist) + 1 == int(State.Public));
+        assert(int(State.Public) + 1 == int(State.Finished));
+    }
 }
