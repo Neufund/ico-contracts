e6b161209e5b507306d53807474f7846c05de2b4
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-10-03 23:51:43 +0200

    comments AccessRoles

--- contracts/AccessRoles.sol
+++ contracts/AccessRoles.sol
@@ -17,11 +17,19 @@ contract AccessRoles {
     // may setup whitelists and abort whitelisting contract with curve rollback
     bytes32 internal constant ROLE_WHITELIST_ADMIN = 0xaef456e7c864418e1d2a40d996ca4febf3a7e317fe3af5a7ea4dda59033bbe5c;
 
+    // May issue (generate) Neumarks
     bytes32 internal constant ROLE_NEUMARK_ISSUER = 0x921c3afa1f1fff707a785f953a1e197bd28c9c50e300424e015953cbf120c06c;
+
+    // May burn Neumarks it owns
     bytes32 internal constant ROLE_NEUMARK_BURNER = 0x19ce331285f41739cd3362a3ec176edffe014311c0f8075834fdd19d6718e69f;
+
+    // May create new snapshots on Neumark
     bytes32 internal constant ROLE_SNAPSHOT_CREATOR = 0x08c1785afc57f933523bc52583a72ce9e19b2241354e04dd86f41f887e3d8174;
+
+    // May enable/disable transfers on Neumark
     bytes32 internal constant ROLE_TRANSFER_ADMIN = 0xb6527e944caca3d151b1f94e49ac5e223142694860743e66164720e034ec9b19;
 
+    // may reclaim tokens/ether from contracts supporting IReclaimable interface
     bytes32 internal constant ROLE_RECLAIMER = 0x0542bbd0c672578966dcc525b30aa16723bb042675554ac5b0362f86b6e97dc5;
 
     // represents legally platform operator in case of forks and contracts with legal agreement attached. keccak256("PlatformOperatorRepresentative")
