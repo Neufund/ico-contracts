b073f7eb2dce152009d48ffd19738708f57fa125
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-10-11 19:49:51 +0200

    moves setAccessPolicy to interface, adds documentation

--- contracts/AccessControl/IAccessControlled.sol
+++ contracts/AccessControl/IAccessControlled.sol
@@ -2,13 +2,15 @@ pragma solidity 0.4.15;
 
 import './IAccessPolicy.sol';
 
-
+/// @title enables access control in implementing contract
+/// @dev see AccessControlled for implementation
 contract IAccessControlled {
 
     ////////////////////////
     // Events
     ////////////////////////
 
+    /// @dev must log on access policy change
     event LogAccessPolicyChanged(
         address controler,
         IAccessPolicy oldPolicy,
@@ -18,6 +20,13 @@ contract IAccessControlled {
     ////////////////////////
     // Public functions
     ////////////////////////
+
+    /// @dev allows to change access control mechanism for this contract
+    ///     this method must be itself access controlled, see AccessControlled
+    /// @notice it is a huge issue for Solidity that modifiers are not part of function signature
+    ///     then interfaces could be used for example to control access semantics
+    function setAccessPolicy(IAccessPolicy newPolicy)
+        public;
 
     function accessPolicy()
         public
