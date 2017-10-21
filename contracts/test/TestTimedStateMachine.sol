e2dc5dc9acc7243b6e3d41be59c25900c314886c
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-10-21 15:56:42 +0200

    implements CHF-33 tests for TimedStateMachine

---
+++ contracts/test/TestTimedStateMachine.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.4.15;
+
+import "../Commitment/TimedStateMachine.sol";
+
+
+contract TestTimedStateMachine is
+    TimedStateMachine
+{
+    ////////////////////////
+    // Constructor
+    ////////////////////////
+
+    function TestTimedStateMachine(int256 whitelistStart)
+        public
+        TimedStateMachine(whitelistStart)
+    {
+    }
+
+    ////////////////////////
+    // Public Methods
+    ////////////////////////
+
+    function testStateOrdering()
+        public
+    {
+        assert(State.Before == State.Before);
+        assert(State.Before < State.Whitelist);
+        assert(State.Before < State.Public);
+        assert(State.Before < State.Finished);
+
+        assert(State.Whitelist > State.Before);
+        assert(State.Whitelist == State.Whitelist);
+        assert(State.Whitelist < State.Public);
+        assert(State.Whitelist < State.Finished);
+
+        assert(State.Public > State.Before);
+        assert(State.Public > State.Whitelist);
+        assert(State.Public == State.Public);
+        assert(State.Public < State.Finished);
+
+        assert(State.Finished > State.Before);
+        assert(State.Finished > State.Whitelist);
+        assert(State.Finished > State.Public);
+        assert(State.Finished == State.Finished);
+
+        assert(State.Public <= State.Public);
+        assert(State.Public >= State.Public);
+
+        assert(int(State.Before) + 1 == int(State.Whitelist));
+        assert(int(State.Whitelist) + 1 == int(State.Public));
+        assert(int(State.Public) + 1 == int(State.Finished));
+    }
+}
