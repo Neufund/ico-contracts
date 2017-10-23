93534a5a542b359ab5d781592e9e2d4a58656df1
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-10-23 14:18:19 +0200

    changes private network id

--- truffle.js
+++ truffle.js
@@ -92,12 +92,13 @@ module.exports = {
       network_id: "*",
       host: "localhost",
       port: 8545,
+      gas: 4600000,
       provider: nanoWeb3Provider() // Our costume instance
     },
     nf_private: {
       host: "localhost",
       port: 8545,
-      network_id: "11",
+      network_id: "16",
       gas: 4600000,
       // gasPrice: 11904761856
       gasPrice: 21000000000
@@ -105,7 +106,7 @@ module.exports = {
     nf_private_test: {
       host: "localhost",
       port: 8545,
-      network_id: "11",
+      network_id: "16",
       gas: 4600000,
       // gasPrice: 11904761856
       gasPrice: 21000000000
