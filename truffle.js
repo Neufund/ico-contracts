3797d1646ffdc4f4dabe9b8accdc4f7e1778fe28
Moe Elsharif <mohammed.elsharif@gmail.com>
2017-10-24 16:15:52 +0200

    Add nano as seperate module
    Add external test script
    Add nano network

--- truffle.js
+++ truffle.js
@@ -1,52 +1,11 @@
 require("babel-register");
 require("babel-polyfill");
 const TestRPC = require("ethereumjs-testrpc");
+const nanoWeb3Provider = require("./nanoWeb3Provider");
 
-/**
- In order to deploy normally without this setup you will have to comment all the code from =====>
- */
-const Web3 = require("web3");
+const providerUrl = "http://localhost:8545";
+const nanoPath = "44'/60'/0'/0`";
 
-function nanoWeb3Provider() {
-
-  const ProviderEngine = require("web3-provider-engine");
-  const LedgerWalletSubproviderFactory = require("ledger-wallet-provider");
-  const Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
-  const FilterSubprovider = require("web3-provider-engine/subproviders/filters.js");
-
-  const providerUrl = "http://localhost:8545";
-  const nanoPath = "44'/60'/0'/0`";
-
-  const web3HttpProvider = new Web3.providers.HttpProvider(providerUrl);
-  const engine = new ProviderEngine();
-
-  engine.addProvider(new FilterSubprovider());
-  engine.addProvider(
-    LedgerWalletSubproviderFactory.default(new Web3(web3HttpProvider), nanoPath)
-  );
-  engine.addProvider(new Web3Subprovider(web3HttpProvider));
-
-  engine.on('block', function (block) {
-    console.log('================================');
-    console.log('BLOCK CHANGED:', '#' + block.number.toString('hex'), '0x' + block.hash.toString('hex'));
-    console.log('================================');
-    engine.stop();
-  })
-  engine.start();
-
-  return engine;
-}
-/**
- =====> Till here
- BUG: truffle cannot finish deployment without commenting the above section
- apparently for some reason something happens with there default instance regardless
- if you don't comment this section smart contracts will not deploy
- TEST CASE:  1 - deploy contracts using truffle's default deployer instance
- 2 - Use Nano engine with truffle console
- 3 - in console commitment = Commitment.at("ICO ADDRESS")
- 4 - in console commitment.amendAgreement("ipfs").then((data) => console.log(data))
- Transaction was signed succseffully
- */
 module.exports = {
   networks: {
     localhost: {
@@ -57,13 +16,13 @@ module.exports = {
     inprocess: {
       network_id: "*",
       provider: TestRPC.provider({
-        accounts: Array(10).fill({balance: "12300000000000000000000000"})
+        accounts: Array(10).fill({ balance: "12300000000000000000000000" })
       })
     },
     inprocess_test: {
       network_id: "*",
       provider: TestRPC.provider({
-        accounts: Array(10).fill({balance: "12300000000000000000000000"})
+        accounts: Array(10).fill({ balance: "12300000000000000000000000" })
       })
     },
     inprocess_massive_test: {
@@ -105,6 +64,12 @@ module.exports = {
       port: 8545,
       gas: 4600000
       // provider: nanoWeb3Provider.nanoWeb3Provider(providerUrl, nanoPath) // Our costume instance
+    },
+    nano: {
+      network_id: "*",
+      host: "localhost",
+      port: 8545,
+      gas: 4600000
     },
     simulated_live: {
       network_id: "*",
