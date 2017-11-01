2f3f696bdaa2079eb71bcfea46cbacd537bca67f
Moe Elsharif <mohammed.elsharif@gmail.com>
2017-11-01 14:40:21 +0100

    Get needed info from config.js
    Move all scripts to ./scripts

--- scripts/prefillAgreements.js
+++ scripts/prefillAgreements.js
@@ -2,29 +2,44 @@
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 require("babel-register");
+const Promise = require("bluebird");
+const getConfig = require("../migrations/config").default;
+
 const {
   prepareReservationAgreement,
   prepareNeumarkTokenHolderAgreement
-} = require("../legal/prepareDocuments");
+} = require("./prepareDocuments");
 
 const Neumark = artifacts.require("Neumark");
 const LockedAccount = artifacts.require("LockedAccount");
 const Commitment = artifacts.require("Commitment");
-
+// eslint-disable-next-line no-unused-vars
 module.exports = async function(callback) {
   const neumark = await Neumark.deployed();
   const commitment = await Commitment.deployed();
   const etherLock = await LockedAccount.at(await commitment.etherLock());
+  const getWeb3Accounts = Promise.promisify(web3.eth.getAccounts);
 
-  await prepareReservationAgreement({
-    neumarkContract: neumark,
-    commitmentContract: commitment,
-    lockedAccountContract: etherLock,
-    companyAddress: "0x83CBaB70Bc1d4e08997e5e00F2A3f1bCE225811F"
-  });
-
-  await prepareNeumarkTokenHolderAgreement({
-    neumarkContract: neumark,
-    companyAddress: "0x83CBaB70Bc1d4e08997e5e00F2A3f1bCE225811F"
-  });
+  try {
+    const configrations = getConfig(
+      web3,
+      artifacts.options._values.network,
+      await getWeb3Accounts()
+    );
+    console.log("Starting: Reservation Agreement");
+    await prepareReservationAgreement({
+      neumarkContract: neumark,
+      commitmentContract: commitment,
+      lockedAccountContract: etherLock,
+      companyAddress: configrations.addresses.PLATFORM_OPERATOR_REPRESENTATIVE
+    });
+    console.log("Starting: Token Holder Agreement");
+    await prepareNeumarkTokenHolderAgreement({
+      neumarkContract: neumark,
+      companyAddress: configrations.addresses.PLATFORM_OPERATOR_REPRESENTATIVE
+    });
+    console.log("Done");
+  } catch (err) {
+    console.log(err);
+  }
 };
