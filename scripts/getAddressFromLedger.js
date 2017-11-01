e7f4a9a24e3b03ae96b76be1b7e5d639a9ec7fe3
banciur <banciur@users.noreply.github.com>
2017-11-01 15:44:38 +0100

    Change name of ledger script to ledgeraddress add logging of errors (#110)
    * Change name of ledger script to ledgeraddress add logging of errors
    
    * getAddressFromLedger.js lint fixes

--- scripts/getAddressFromLedger.js
+++ scripts/getAddressFromLedger.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 /**
  * Simple script to get address from ledger using different derivation paths
- * usage: yarn ledger "44'/60'/0'/1" if you leave it empty you will get default path
+ * usage: yarn ledgeraddresss "44'/60'/0'/1" if you leave it empty you will get default path
  */
 // eslint-disable-next-line import/no-extraneous-dependencies
 const ledger = require("ledgerco");
@@ -12,11 +12,17 @@ const DEFAULT_PATH = "44'/60'/0'/0";
 const path = process.argv[2] || DEFAULT_PATH;
 console.log("checking path:", path);
 
-ledger.comm_node.create_async(TIMEOUT).then(comm => {
-  // eslint-disable-next-line new-cap
-  const eth = new ledger.eth(comm);
-  return eth.getAddress_async(path).then(result => {
-    console.log(result);
-    comm.close_async();
-  });
-});
+ledger.comm_node
+  .create_async(TIMEOUT)
+  .then(comm => {
+    // eslint-disable-next-line new-cap
+    const eth = new ledger.eth(comm);
+    return eth
+      .getAddress_async(path)
+      .then(result => {
+        console.log(result);
+        comm.close_async();
+      })
+      .catch(err => console.log(err));
+  })
+  .catch(err => console.log(err));
