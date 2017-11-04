9eaa6d46f2c911673e631b23f3dfa6e2fd2f74af
Krzysztof Kaczor <chris@kaczor.io>
2017-11-04 18:21:45 +0100

    revert ethertoken removal

---
+++ contracts/EtherToken.sol
@@ -0,0 +1,114 @@
+pragma solidity 0.4.15;
+
+import './AccessControl/AccessControlled.sol';
+import './Reclaimable.sol';
+import "./IsContract.sol";
+import './Standards/IERC223Token.sol';
+import './Standards/IERC223Callback.sol';
+import './SnapshotToken/Helpers/TokenMetadata.sol';
+import './Zeppelin/StandardToken.sol';
+
+
+contract EtherToken is
+    IsContract,
+    AccessControlled,
+    StandardToken,
+    TokenMetadata,
+    Reclaimable
+{
+    ////////////////////////
+    // Constants
+    ////////////////////////
+
+    string private constant NAME = "Ether Token";
+
+    string private constant SYMBOL = "ETH-T";
+
+    uint8 private constant DECIMALS = 18;
+
+    ////////////////////////
+    // Events
+    ////////////////////////
+
+    event LogDeposit(
+        address indexed to,
+        uint256 amount
+    );
+
+    event LogWithdrawal(
+        address indexed from,
+        uint256 amount
+    );
+
+    ////////////////////////
+    // Constructor
+    ////////////////////////
+
+    function EtherToken(IAccessPolicy accessPolicy)
+        AccessControlled(accessPolicy)
+        StandardToken()
+        TokenMetadata(NAME, DECIMALS, SYMBOL, "")
+        Reclaimable()
+    {
+    }
+
+    ////////////////////////
+    // Public functions
+    ////////////////////////
+
+    /// deposit msg.value of Ether to msg.sender balance
+    function deposit()
+        payable
+        public
+    {
+        _balances[msg.sender] = add(_balances[msg.sender], msg.value);
+        _totalSupply = add(_totalSupply, msg.value);
+        LogDeposit(msg.sender, msg.value);
+        Transfer(address(0), msg.sender, msg.value);
+    }
+
+    /// withdraws and sends 'amount' of ether to msg.sender
+    function withdraw(uint256 amount)
+        public
+    {
+        require(_balances[msg.sender] >= amount);
+        _balances[msg.sender] = sub(_balances[msg.sender], amount);
+        _totalSupply = sub(_totalSupply, amount);
+        msg.sender.transfer(amount);
+        LogWithdrawal(msg.sender, amount);
+        Transfer(msg.sender, address(0), amount);
+    }
+
+    //
+    // Implements IERC223Token
+    //
+
+    function transfer(address to, uint256 amount, bytes data)
+        public
+        returns (bool)
+    {
+        transferInternal(msg.sender, to, amount);
+
+        // Notify the receiving contract.
+        if (isContract(to)) {
+            // in case of re-entry (1) transfer is done (2) msg.sender is different
+            IERC223Callback(to).onTokenTransfer(msg.sender, amount, data);
+        }
+        return true;
+    }
+
+    //
+    // Overrides Reclaimable
+    //
+
+    /// @notice allows EtherToken to reclaim tokens wrongly sent to its address
+    /// @dev as EtherToken by design has balance of Ether (native Ethereum token)
+    ///     such reclamation is not allowed
+    function reclaim(IBasicToken token)
+        public
+    {
+        // forbid reclaiming ETH hold in this contract.
+        require(token != RECLAIM_ETHER);
+        Reclaimable.reclaim(token);
+    }
+}
