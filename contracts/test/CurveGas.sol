5d0cd007aa51c29c046085a0e2a82eeccbd504c6
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-11-05 02:49:23 +0100

    extends CurveGas contract with new wrappers

--- contracts/test/CurveGas.sol
+++ contracts/test/CurveGas.sol
@@ -11,10 +11,44 @@ contract CurveGas is NeumarkIssuanceCurve {
 
     function cumulativeWithGas(uint256 n)
         external
+        constant
         returns (uint256, uint256)
     {
         uint256 start = msg.gas;
         uint256 result = cumulative(n);
+        uint256 finish = msg.gas;
+        return (result, start - finish);
+    }
+
+    function incrementalInverseWithGas(uint256 totalEuroUlps, uint256 neumarkUlps)
+        external
+        constant
+        returns (uint256, uint256)
+    {
+        uint256 start = msg.gas;
+        uint256 result = incrementalInverse(totalEuroUlps, neumarkUlps);
+        uint256 finish = msg.gas;
+        return (result, start - finish);
+    }
+
+    function incrementalInverseWithGas(uint256 totalEuroUlps, uint256 neumarkUlps, uint256 minEurUlps, uint256 maxEurUlps)
+        external
+        constant
+        returns (uint256, uint256)
+    {
+        uint256 start = msg.gas;
+        uint256 result = incrementalInverse(totalEuroUlps, neumarkUlps, minEurUlps, maxEurUlps);
+        uint256 finish = msg.gas;
+        return (result, start - finish);
+    }
+
+    function cumulativeInverseWithGas(uint256 neumarkUlps, uint256 minEurUlps, uint256 maxEurUlps)
+        external
+        constant
+        returns (uint256, uint256)
+    {
+        uint256 start = msg.gas;
+        uint256 result = cumulativeInverse(neumarkUlps, minEurUlps, maxEurUlps);
         uint256 finish = msg.gas;
         return (result, start - finish);
     }
